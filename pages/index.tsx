bf154373a3cfa7ba1b6b1ba169b508bb9ccc4e70
ririgireddy <ririgireddy1@gmail.com>
2022-06-14 12:06:09 -0700

    margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
 
-<div className=" grid justify-center mt-40">
+<div className=" grid justify-center mt-28">
   <div className="relative">
     <div className="absolute -inset-0.5 bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt" />
     <div className="max-w-sm relative px-7 py-4 bg-gray-900 rounded-3xl">
